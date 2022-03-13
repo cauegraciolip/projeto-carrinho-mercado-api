2aa5c9e3d1b0742af8c67ca37e62dbfe6aee87cb
cauegraciolip <cauegracioli@outlook.com>
2022-03-13 00:22:23 -0300

    feat: add more dependences to project, conn w/ db

---
+++ prisma/migrations/20220313020726_create_table_user_lista/migration.sql
@@ -0,0 +1,30 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Lista" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "title" VARCHAR(255) NOT NULL,
+    "content" JSONB[],
+    "userOwner" INTEGER NOT NULL,
+
+    CONSTRAINT "Lista_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Lista" ADD CONSTRAINT "Lista_userOwner_fkey" FOREIGN KEY ("userOwner") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
