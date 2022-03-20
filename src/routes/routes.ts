59c9762d0200dae4982e6122dd77090131c7f48a
cauegraciolip <cauegracioli@outlook.com>
2022-03-20 20:50:59 -0300

    feat: add new route to authenticate a user

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,11 +1,16 @@
 import { Router } from "express";
 
 import { userCreate } from "../modules/UserCreate/UserCreateController";
+import { loginUser } from "../modules/UserLogin/UserLoginController";
 
 const routes = Router();
 
 routes.post("/users", (req, res) => {
-    userCreate(req, res);
+    return userCreate(req, res);
+})
+
+routes.post("/login", (req, res) => {
+    return loginUser(req, res);
 })
 
 export { routes }
